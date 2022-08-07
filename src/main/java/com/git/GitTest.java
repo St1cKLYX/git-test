c01bce10e087e5daa91bed7bcfb08aa313d1d6c9
st1ckl <st1cklyx@gmail.com>
2022-08-07 09:54:15 +0800

    first commit

---
+++ src/main/java/com/git/GitTest.java
@@ -0,0 +1,11 @@
+package com.git;
+
+/**
+ * @Author：St1cKL
+ * @Date: 2022/8/7
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+    }
+}
